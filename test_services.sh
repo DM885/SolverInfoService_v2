851d9ef70cbcda763f4b53027146a84ac813ae06
yosou20 <yosou20@student.sdu.dk>
2022-02-18 11:49:02 +0100

    enabled CICD

--- test_services.sh
+++ test_services.sh
@@ -7,9 +7,9 @@
 kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/GatewayService/main/deployment.yaml
 
 # CRUD SERVICE
-# kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/CRUDservice/main/deployment.yaml
+ kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/CRUDservice/main/deployment.yaml
 
-# SOLVER INFO SERVICE
+ SOLVER INFO SERVICE
 # kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/SolverInfoService/main/deployment.yaml
 
 # AUTH SERVICE
@@ -22,4 +22,4 @@ kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/Authenticati
 # kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/JobService/main/deployment.yaml
 
 # MINIZINC SERVICE
-# kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/MiniZincService/main/deployment.yaml
+ kubectl -n rabbits apply -f https://raw.githubusercontent.com/DM885/MiniZincService/main/deployment.yaml
